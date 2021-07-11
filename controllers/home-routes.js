977d485800accc929f4ea4fe42bf02d64df9d4f7
AustinJohnAaro <austin1472a@gmail.com>
2021-07-11 15:06:20 -0700

    fghgg

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,97 +5,100 @@ var { Post, User, Comment } = require('../models');
 // get all posts for homepage
 router.get('/', (req, res) => {
   console.log('======================');
-  Post.findAll({
-    attributes: [
-      'id',
-      'post_text',
-      'title',
-      'created_at'
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      var posts = dbPostData.map(post => post.get({ plain: true }));
+  res.render("dashboard");
+})
 
-      res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn,
-        username: req.session.username
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+//   Post.findAll({
+//     attributes: [
+//       'id',
+//       'post_text',
+//       'title',
+//       'created_at'
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//         include: {
+//           model: User,
+//           attributes: ['username']
+//         }
+//       },
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//   })
+//     .then(dbPostData => {
+//       var posts = dbPostData.map(post => post.get({ plain: true }));
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
+//       res.render('homepage', {
+//         posts,
+//         loggedIn: req.session.loggedIn,
+//         username: req.session.username
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-  res.render('login');
-});
+// router.get('/login', (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect('/');
+//     return;
+//   }
 
-router.get('/post/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'post_text',
-      'title',
-      'created_at',
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      // serialize the data
-      var post = dbPostData.get({ plain: true })
+//   res.render('login');
+// });
 
-      // pass data to template
-      res.render('single-post', {
-        post,
-        loggedIn: req.session.loggedIn,
-        username: req.session.username
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.get('/post/:id', (req, res) => {
+//   Post.findOne({
+//     where: {
+//       id: req.params.id
+//     },
+//     attributes: [
+//       'id',
+//       'post_text',
+//       'title',
+//       'created_at',
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//         include: {
+//           model: User,
+//           attributes: ['username']
+//         }
+//       },
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//   })
+//     .then(dbPostData => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: 'No post found with this id' });
+//         return;
+//       }
+//       // serialize the data
+//       var post = dbPostData.get({ plain: true })
+
+//       // pass data to template
+//       res.render('single-post', {
+//         post,
+//         loggedIn: req.session.loggedIn,
+//         username: req.session.username
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// }); 
 
 module.exports = router; 
